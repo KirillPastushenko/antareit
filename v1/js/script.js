98d7939d396855466080ece98d007582aa9bac0a
KirillPastushenko <kpastushenko@gmail.com>
2024-12-30 20:15:07 +0300

    добавлена v2

---
+++ v1/js/script.js
@@ -0,0 +1,33 @@
+ 
+const menuButton = document.querySelector(".btn-header-menu");
+const menu = document.querySelector(".header-menu");
+const body = document.querySelector("body");
+
+let menuOpened = false;
+
+ 
+menuButton.addEventListener("click", () => {
+ 
+  menuOpened = !menuOpened;
+ 
+  if (menuOpened) {
+    menuButton.classList.add("opened");
+    menu.classList.add("opened");
+	body.classList.add("fixed");
+  } else {
+    menuButton.classList.remove("opened");
+    menu.classList.remove("opened");
+    body.classList.remove("fixed");
+  }
+});
+
+$(document).ready(function(){
+  $('.text-block-btn').on('click', function(){
+    $(this).parent().toggleClass('active');
+  })
+
+  $('.effect-text-btn').on('click', function(){
+    $(this).closest('.effect-text-block').toggleClass('active');
+  })
+
+})
