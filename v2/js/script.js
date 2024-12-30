3bc4e21f1dac2c081e222bcad23f879091df403f
KirillPastushenko <kpastushenko@gmail.com>
2024-12-30 21:01:50 +0300

    добавлен скролл при клике по ссылке

--- v2/js/script.js
+++ v2/js/script.js
@@ -30,4 +30,17 @@ $(document).ready(function(){
     $(this).closest('.effect-text-block').toggleClass('active');
   })
 
+
+  $('a[href^="#"]').on('click', function(event) {
+    event.preventDefault();
+
+    var target = $(this.getAttribute('href'));
+
+    if( target.length ) {
+        $('html, body').animate({
+            scrollTop: target.offset().top
+        }, 800);  
+    }
+});
+
 })
